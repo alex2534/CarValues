f42f464eb3d211b3e37fa9e956b3817fac4f8663
alex2534 <alex2534@outlook.com>
2023-09-08 15:52:13 -0300

    Done

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,44 +1,42 @@
-import {useDispatch, useSelector} from "react-redux";
-import {removeCar} from "../store";
-
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from "../store";
 
 function CarList() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  // Using distructoring to get the data we need
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    // This is a direved state
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    return { cars: filteredCars, name: form.name };
+  });
 
-    const cars = useSelector((state) => {
-        return state.cars.data;
-    })
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
 
-    const handleCarDelete = (car) => {
-        dispatch(removeCar(car.id))
+  const renderCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
 
-    }
+    return (
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
+        <p>
+          {" "}
+          {car.name}- ${car.cost}{" "}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
 
-    const renderCars = cars.map((car) => {
-        return (
-            <div key={
-                    car.id
-                }
-                className="panel">
-                <p> {
-                    car.name
-                }
-                    - ${
-                    car.cost
-                } </p>
-                <button className="button is-danger"
-                    onClick={
-                        () => handleCarDelete(car)
-                }>Delete</button>
-            </div>
-        )
-    })
-
-
-    return <div className="car-list">
-        {renderCars}</div>
-
+  return <div className="car-list">{renderCars}</div>;
 }
 
 export default CarList;
--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,9 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
 
 
+function CarValue() {
 
+    const totalCost = useSelector(({ cars: { data,
+        searchTerm
+    }
+    }) => {
+        const filteredCars = data.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+        // The inicial value of the acc is 0
+        return filteredCars.reduce((acc, car) => {
+            return acc + car.cost;
+        }, 0);
+    });
 
-function CarValue () {
-    return <div>CarValue</div>
+    return <div className="car-value">Total cost: ${totalCost}</div>
 }
 
 export default CarValue;
